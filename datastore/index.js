1a6e9777249faa3c6d4630ee57969a8914f5e27c
Zechariah Deckert-Holscher <zdeckert@gmail.com>
2020-04-13 18:58:12 -0700

    delete

--- datastore/index.js
+++ datastore/index.js
@@ -76,22 +76,33 @@ exports.update = (id, text, callback) => {
         } else {
           callback(null, {id, text});
         }
-      })
+      });
     }
   });
 };
 
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.unlink(`${exports.dataDir}/${id}.txt`, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
 };
+
+// exports.delete = (id, callback) => {
+//   var item = items[id];
+//   delete items[id];
+//   if (!item) {
+//     // report an error if item not found
+//     callback(new Error(`No item with id: ${id}`));
+//   } else {
+//     callback();
+//   }
+// };
+
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
